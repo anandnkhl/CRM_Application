bafbc4c6871fbd3438c773501dc4dbdda74447d3
nikhil <anandnikhil91@gmail.com>
2020-01-04 18:08:40 +0530

    dashboard added

--- nodeServer.js
+++ nodeServer.js
@@ -10,47 +10,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/verifyUser', function(req, res) {
 	var usrID =  parseInt(req.body.UserId, 10);
 	var pas = req.body.Password;
-     var varified = 0;
+     var verified = "no";
      var query = {$and:[ {UID : usrID}, {Password : pas}]};
 	mongo.connect(url,  {
-             useNewUrlParser: true,
-             useUnifiedTopology: true
-     },function(err, db) {
+          useNewUrlParser: true, useUnifiedTopology: true
+          },function(err, db) {
                if (err) throw err;
                var dbo = db.db("CRMDatabase");
                dbo.collection("CRMUser").find(query).toArray(function(err, result) {
                     if (err) throw err;
                     if(result.length != 0){
-                         console.log("Verified User");
+                         console.log('yes');
+                         verified = "yes";
                     }else{
-                         console.log("Invalid User");
+                         console.log('no');
+                         verified = "no";
                     }
-                db.close();
+                    db.close();
+                    if( verified == "no"){
+                         console.log('----->no');
+                         return res.sendFile( __dirname + '/start.html');
+                    }else if(verified == "yes"){
+                         console.log('----->yes');
+                         return res.sendFile( __dirname + '/dashboard.html');
+                    }
                });
-           });
+     });
 });
- //     var output = dbo.collection("CRMUser").count().toArray;
- //     	if(output != 0){
- //     		console.log("User is valid", output);
- //     	}else{
- //     		console.log("Invalid User", output);
- //     	}
- //     db.close();
-	// });
+
+
 
 app.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:8080/');
 });
-
-
-
-// mongo.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("CRMDatabase");
-//   var myobj = { UID : 2, Name: "Rajesh", Email : "rajesh@crm.com", Phone : 8947562136, Password : "ABCDE"};
-//   dbo.collection("CRMUser").insertOne(myobj, function(err, res) {
-//     if (err) throw err;
-//     console.log("1 document inserted");
-//     db.close();
-//   });
-// });
